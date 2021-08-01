b4422dcdb38a0fc79093aa2cd29491dd203b240c
nitish7890 <82252897+nitish7890@users.noreply.github.com>
2021-08-01 10:37:34 +0530

    Create new-payroll.js

---
+++ new-payroll.js
@@ -0,0 +1,77 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name=document.querySelector('#name');
+    const textError=document.querySelector('.text-error');
+    name.addEventListener('input',function() {
+        if(name.Value.length==0) {
+            textError.textContent="";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name=name.value;;
+            textError.textContent="";
+
+        }catch (e){
+            textError.textContent=e;
+        }
+    });
+
+    const salary=document.querySelector('#salary');
+    const output=document.querySelector('.salary-output');
+    output.textContent=salary.value;
+    salary.addEventListener('input',function() {
+        output.textContent=salary.value;
+    });
+});
+
+const save= () => {
+    try {
+        let employeePayrollData=createEmployeePayroll();
+        createAndUpdatesStorage(employeePayrollData);
+
+    }catch (e) {
+        return;
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData=new EmployeePayrollData();
+    try {
+    
+        employeePayrollData.name=getInputValueById('#name');
+    } catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+    employeePayrollData.profilePic = getSelectedValues( '[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getSelectedValuesById('#salary');
+    employeePayrollData.note = getSelectedValuesById('#notes');
+    let date = getSelectedValuesById('#day')+" "+getSelectedValuesById('#month')+" "+getSelectedValuesById('#year');
+    employeePayrollData.date = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let setItems = [];
+    allItems.forEach(item => {
+        if(item.checked) setItems.push(item.value);
+    });
+    return setItems;
+}
+
+
+
+
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
